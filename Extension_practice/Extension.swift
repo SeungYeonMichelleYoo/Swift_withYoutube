8832c89f286531d23b18ff63b86491c1ae5aa966
michelle <lune111200@gmail.com>
2022-04-24 08:13:39 +0900

    extension practic

---
+++ Extension_practice/Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Extension.swift
+//  Extension_practice
+//
+//  Created by SeungYeon Yoo on 2022/04/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController{
+    
+    func sayHi() {
+        print("하이!")
+    }
+}
---
+++ Extension_practice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Extension_practice
+//
+//  Created by SeungYeon Yoo on 2022/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        sayHi()
+    }
+
+
+
+}
+
