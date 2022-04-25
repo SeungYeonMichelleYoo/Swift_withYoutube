81f4f8d9cb0bcbf09ea33b012525efe180e70da3
michelle <lune111200@gmail.com>
2022-04-25 14:09:05 +0900

    navigation bar item using storyboard- swift

---
+++ NavigationBarItem_Storyboard_tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationBarItem_Storyboard_tutorial
+//
+//  Created by SeungYeon Yoo on 2022/04/25.
+//
+
+import UIKit
+
+class ViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
