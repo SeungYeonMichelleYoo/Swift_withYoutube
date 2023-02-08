dde93743e704074289acdf7521b7d6252ae10330
michelle <lune111200@gmail.com>
2023-02-08 22:52:06 +0900

    tableviewcell xib setting

---
+++ DynamicTableView/DynamicTableView/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  DynamicTableView
+//
+//  Created by SeungYeon Yoo on 2023/02/08.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ DynamicTableView/DynamicTableView/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  DynamicTableView
+//
+//  Created by SeungYeon Yoo on 2023/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
+        cell.descriptionLabel.text = "fsdfasfasfasdfasdfasdfadfasdfasdf"
+        return cell
+    }
+
+}
